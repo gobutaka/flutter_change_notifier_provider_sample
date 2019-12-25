5c3763f4310b1ce3887bd0c567b6e68572fed167
gobutaka <takaaki.hirakura@game8.jp>
2019-12-25 14:47:24 +0900

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,54 +11,81 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ChangeNotifierProvider<Counter>(
+        create: (_) => Counter(0),
+        child: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class Counter with ChangeNotifier {
+  int _value;
+
+  Counter(this._value);
+
+  int get value => this._value;
+
+  void increment() {
+    _value++;
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            WidgetCenterText(),
+            WidgetNumText(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      floatingActionButton: WidgetIncrementBtn(),
+    );
+  }
+}
+
+class WidgetCenterText extends StatelessWidget {
+  const WidgetCenterText({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Text('You have pushed the button this many times:');
+  }
+}
+
+class WidgetNumText extends StatelessWidget {
+  const WidgetNumText({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
+    return Text(
+      '${counter.value}',
+      style: Theme.of(context).textTheme.display1,
+    );
+  }
+}
+
+class WidgetIncrementBtn extends StatelessWidget {
+  const WidgetIncrementBtn({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context, listen: false);
+    return FloatingActionButton(
+      onPressed: () => counter.increment(),
+      tooltip: 'Increment',
+      child: Icon(Icons.add),
     );
   }
 }
